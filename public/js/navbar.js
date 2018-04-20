6b8328a0facd7af7555858f42987946231da2fa5
Shomari Malcolm <shomarimalcolm@gmail.com>
2018-04-20 11:54:49 -0400

    Fixed some UI bugs

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -33,20 +33,3 @@ if($(".inView").is(":visible")){
     $(".inView").addClass(" animated bounceInLeft");
 }
 
-var skillWrapper = document.getElementsByClassName("skill-wrapper")[0];
-var skillHeaderContent = document.getElementsByClassName("skill-header-content")[0];
-var contactSection = document.getElementById("contact-section")
-var contactInfo = document.getElementsByClassName("contact-info")[0];
-
-skillWrapper.addEventListener("mouseover", () => {
-    skillWrapper.classList.add("animated")
-    skillWrapper.classList.add("zoomIn")
-    skillHeaderContent.classList.add("animated")
-    skillHeaderContent.classList.add("zoomIn")
-})
-
-contactSection.addEventListener("mouseover", () => {
-    contactInfo.classList.add("animated")
-    contactInfo.classList.add("zoomIn")
-})
-
